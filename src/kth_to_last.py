ebd939d899ed9c7650d380fae2f97205244e6470
isaque <isaque.gomes@upe.br>
2026-05-10 11:19:25 -0300

    Finalizando as questoes ate a 7

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -2,4 +2,19 @@ from src.my_node import MyNode
 
 
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    if head is None or k <= 0:
+        return -1
+
+    first = head
+    second = head
+
+    for _ in range(k):
+        if second is None:
+            return -1
+        second = second.next
+
+    while second is not None:
+        first = first.next
+        second = second.next
+
+    return first.value
