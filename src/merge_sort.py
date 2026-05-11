f24181eb15b31575930bea5af2705359910fda7c
isaque <isaque.gomes@upe.br>
2026-05-11 20:36:22 -0300

    Finalizando a lista

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -2,4 +2,49 @@ from src.my_array import MyArray
 
 
 def merge_sort(array: MyArray) -> MyArray:
-    raise NotImplementedError
+
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+
+    left = MyArray()
+    right = MyArray()
+
+    for i in range(mid):
+        left.append(array[i])
+
+    for i in range(mid, len(array)):
+        right.append(array[i])
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left: MyArray, right: MyArray) -> MyArray:
+
+    resultado = MyArray()
+
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+
+        if left[i] < right[j]:
+            resultado.append(left[i])
+            i += 1
+        else:
+            resultado.append(right[j])
+            j += 1
+
+    while i < len(left):
+        resultado.append(left[i])
+        i += 1
+
+    while j < len(right):
+        resultado.append(right[j])
+        j += 1
+
+    return resultado
--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,5 +1,35 @@
 from src.my_array import MyArray
 
-
 def quick_sort(array: MyArray) -> MyArray:
-    raise NotImplementedError
+
+    quick(array, 0, len(array) - 1)
+
+    return array
+
+def quick(array: MyArray, low: int, high: int):
+
+    if low < high:
+
+        pivot_index = partition(array, low, high)
+
+        quick(array, low, pivot_index - 1)
+
+        quick(array, pivot_index + 1, high)
+
+def partition(array: MyArray, low: int, high: int) -> int:
+
+    pivot = array[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+
+        if array[j] <= pivot:
+
+            i += 1
+
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
